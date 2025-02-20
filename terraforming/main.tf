b8cd07a91e8ab3eea457df9b918de8d507c80603
agiza1 <89607892+agiza1@users.noreply.github.com>
2025-02-20 17:33:08 -0500

    Update main.tf

--- terraforming/main.tf
+++ terraforming/main.tf
@@ -6,13 +6,6 @@
      resource "aws_s3_bucket" "s3_bucket" {
       bucket = "${var.bucket_name}"
       
-     
-       lifecycle_rule {
-         enabled = true
-         expiration {
-           days = 1
-         }
-       }
      }
 
      variable "region" {}
