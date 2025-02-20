fe91c5005575d5721b61ae9b747ae000676e3b07
agiza1 <89607892+agiza1@users.noreply.github.com>
2025-02-20 18:53:28 -0500

    Update main.tf

--- terraforming/main.tf
+++ terraforming/main.tf
@@ -5,10 +5,7 @@
 
      resource "aws_s3_bucket" "env0-bucket" {
       bucket = "${var.bucket_name}"
-      tags {
-         Name = "${var.bucket_name}"
-         env  = "${var.env}"
-      }
+      
      }
 
      variable "region" {}
