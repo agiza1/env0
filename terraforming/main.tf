431a90af33cf53ef9f66a811dcc998c4a5ca1d91
agiza1 <89607892+agiza1@users.noreply.github.com>
2025-02-21 15:26:12 -0500

    Update main.tf

--- terraforming/main.tf
+++ terraforming/main.tf
@@ -3,7 +3,7 @@
        region = "us-west-2" 
      }
 
-     resource "aws_s3_bucket" "env0-bucket" {
+     resource "aws_s3_bucket" "env0-buckets" {
       bucket = "${var.bucket_name}"
 
      variable "region" {}
